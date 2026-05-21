46b242fd11f7d819531a79cb2ae100998816fc64
Anas Anabusy <58163890+Ann434@users.noreply.github.com>
2026-05-21 11:31:10 +0300

    Remove auth middleware from GET users route
    Removed authentication middleware from GET users route.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -73,7 +73,7 @@ router.post('/login', async (req, res) => {
 });
 
 // ======================= GET USERS =====================
-router.get('/', auth, async (req, res) => {
+router.get('/', async (req, res) => {
     const snapshot = await db.collection('users').get();
 
     const users = snapshot.docs.map(doc => {
